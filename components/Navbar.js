684d85c848e019c51d6fa0331df65d6f90226dad
Mahapara Naim <mahaparanaim57@gmail.com>
2023-07-29 03:07:11 +0600

    fixed navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="flex justify-center mx-auto bg-slate-900 py-8">
+    <nav className="flex justify-center fixed top-0 w-full mx-auto bg-slate-900 py-8">
       <h1 className="text-white font-mono font-extrabold text-3xl">LiveChat</h1>
     </nav>
   );
